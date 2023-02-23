e5a893edcb4464adf8c920541c8602372ba61f35
bim121 <118322970+bim121@users.noreply.github.com>
2023-02-23 20:46:36 +0300

    second

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { FileModule } from "./file/file.module";
 import { TrackModule } from "./track/track.module";
+import * as path from 'path'
 
 @Module({
     imports: [
+        ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
         MongooseModule.forRoot('mongodb+srv://bim121:admin@cluster0.ezwkcaw.mongodb.net/?retryWrites=true&w=majority'),
-        TrackModule
+        TrackModule, 
+        FileModule
     ]
 })
 export class AppModule{
--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,6 +1,11 @@
-import { Controller, Get } from "@nestjs/common";
-import { Post } from "@nestjs/common/decorators/http/request-mapping.decorator";
-import { Body } from "@nestjs/common/decorators/http/route-params.decorator";
+import { Controller, Get, UseInterceptors, UploadedFile, UploadedFiles, Query } from "@nestjs/common";
+import { Delete, Post } from "@nestjs/common/decorators/http/request-mapping.decorator";
+import { Body, Param } from "@nestjs/common/decorators/http/route-params.decorator";
+import { FileFieldsInterceptor } from "@nestjs/platform-express";
+import { FilesInterceptor } from "@nestjs/platform-express";
+import { ObjectId } from "mongoose";
+import { off } from "process";
+import { CreateCommentDto } from "./dto/create-comment-dto";
 import { CreateTrackDto } from "./dto/create-track-dto";
 import { TrackService } from "./track.service";
 
@@ -11,20 +16,47 @@ export class TrackController{
 
     }
     @Post()
-    create(@Body() dto: CreateTrackDto){
-        return this.trackService.create(dto);
+    @UseInterceptors(FileFieldsInterceptor([
+        { name: 'picture', maxCount: 1 },
+        { name: 'audio', maxCount: 1 },
+    ]))
+    create(@UploadedFiles() files, @Body() dto: CreateTrackDto): Promise<import("d:/programming/javaScript/fullstack/spotify/server/src/track/schemas/track.schemas").Track>{
+        const {picture, audio} = files;
+        return this.trackService.create(dto, picture[0], audio[0]);
     }
 
     @Get()
-    getAll(){
-        return this.trackService.getAll();
+    getAll(@Query('count') count: number,
+    @Query('offset') offset: number){
+        return this.trackService.getAll(count, offset);
     }
 
-    getOne(){
-
+    @Get('/search')
+    search(@Query('query') query){
+        return this.trackService.search(query);
     }
 
-    delete(){
-        
+    @Get(':id')
+    getOne(@Param('id') id: ObjectId){
+        return this.trackService.getOne(id);
     }
+
+    @Delete(':id')
+    delete(@Param('id') id:ObjectId){
+        return this.trackService.delete(id);
+    }
+
+    @Post('/comment')
+    addComment(@Body() dto: CreateCommentDto){
+        return this.trackService.addComment(dto);
+    }
+
+    @Post('/listen/:id')
+    listen(@Param('id') id: ObjectId){
+        return this.trackService.listen(id);
+    }
+}
+
+function UploadFiles() {
+    throw new Error("Function not implemented.");
 }
